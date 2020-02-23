62aea8602c7b561dde959b5306b61f8d6c6d5692
wallzack <vulcheek@gmail.com>
2020-02-23 15:21:43 +0100

    Added letters in playView

--- game.js
+++ game.js
@@ -3,10 +3,19 @@ console.log('game script loaded');
 const gameContent = document.getElementById('gameContent');
 gameContent.textContent = '';
 
+const allLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+
+
 const gameState = {
   name: '',
   activeView: 'welcome', 
+  selectedLetters: [],
 };
+
+function stateUpdate(newGameState) {
+  Object.assign(gameState, newGameState);
+  render();
+}
 
 function welcomeView () {
   const header = document.createElement('h1');
@@ -14,9 +23,9 @@ function welcomeView () {
 
   const nameInput = document.createElement('input');
   nameInput.placeholder = 'Enter your name';
+  
   nameInput.addEventListener('input', (event) => {
-    gameState.name = event.target.value;
-    console.log(name);
+    stateUpdate({name: event.target.value});
   });
 
   const nameInputLabel = document.createElement('div');
@@ -24,9 +33,13 @@ function welcomeView () {
   const playButton = document.createElement('button');
   playButton.textContent = 'Play game!';
   playButton.addEventListener('click', () => {
-    gameState.activeView = 'play';
-    render();
+    stateUpdate({ activeView: 'play', selectedLetters: [] });
   });
+
+  setTimeout( () => {
+    nameInput.value = gameState.name;
+    nameInput.focus();
+  }, 0);
 
   gameContent.appendChild(header);
   gameContent.appendChild(nameInputLabel);
@@ -39,14 +52,29 @@ function playView() {
   const header = document.createElement('h1');
   header.textContent = 'Hi ' + gameState.name + '!';
 
+  const buttonsContainer = document.createElement('div');
+  for (let i =0; i < allLetters.length; i++) {
+    const letterButton = document.createElement('button');
+    const letter = allLetters[i];
+    letterButton.textContent = letter;
+    letterButton.disabled = gameState.selectedLetters.includes(letter);
+
+    letterButton.addEventListener('click', () => {
+      console.log(allLetters[i]);
+      stateUpdate({selectedLetters: gameState.selectedLetters.concat([letter])});
+    });
+
+    buttonsContainer.appendChild(letterButton);
+  };
+
   const endGameButton = document.createElement('button');
   endGameButton.textContent = 'End game!';
   endGameButton.addEventListener('click', () => {
-    gameState.activeView = 'endGame';
-    render();
+    stateUpdate({ activeView: 'endGame' });
   });
 
   gameContent.appendChild(header);
+  gameContent.appendChild(buttonsContainer);
   gameContent.appendChild(endGameButton);
 };
 
